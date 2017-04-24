fa69fa8eda9180d8283b2f1330697be8a7b101dc
Wynand <groendrakie@gmail.com>
2017-04-24 11:07:48 +0200

    refactor and add additional defaults

--- imports/ui/components/insertExample/insertExample.js
+++ imports/ui/components/insertExample/insertExample.js
@@ -3,3 +3,11 @@
 import './insertExample.html';
 
 console.log('load insert example ui component');
+
+Template.insertExample.helpers({
+  defaults: {
+    simpleStringInitialised: 'Initial text value',
+    dateInitialised: new Date(),
+    timeInitialised: '12:34 PM'
+  }
+});
